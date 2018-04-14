148afc0a2f5d73dc2e0aafdcb3ef9a43069cf2da
Miles Grover <miles@milesgrover.com>
2018-04-13 20:04:47 -0700

    functionality working and beginning styles

--- airports.js
+++ airports.js
@@ -44,29 +44,91 @@ IntentMedia.Distances = (function () {
 // Code by Miles Grover
 
 // Replace select controls with elements that can be styled in CSS
-function selectReplace() {
+(function selectReplace() {
     var selectComponents = document.querySelectorAll('.airport-selector');
+    var selectComponent,
+        selectElement,
+        optionElements,
+        entryPoint,
+        optsList,
+        listItem;
 
     // loop through selectComponents nodelist
+    // using for loops since not all browsers support methods like 'foreach' on non-arrays
     for (var i = 0; i < selectComponents.length; i++) {
-        var selectElement = selectComponents[i].querySelector('select');
-        var optionElements = selectElement.querySelectorAll('option');
+        // define elements
+        selectComponent = selectComponents[i];
+        selectElement = selectComponent.querySelector('select');
+        optionElements = selectElement.querySelectorAll('option');
+
+        // hide the select HTML element
         selectElement.style.display = 'none';
 
-        var entryPoint = document.createElement('button');
+        // create new entry point
+        entryPoint = document.createElement('button');
         entryPoint.classList.add('entry-point');
         entryPoint.textContent = optionElements[0].value;
+        entryPoint.addEventListener('click', function(e) {
+            selectOpenList(e);
+        });
         selectComponents[i].appendChild(entryPoint);
 
-        var optsList = document.createElement('ul');
+        // create new options list
+        optsList = document.createElement('ul');
         selectComponents[i].appendChild(optsList);
 
-        // loop through options and add them to a list
+        // loop through options and add them to the list
         for (var j = 0; j < optionElements.length; j++) {
-            var listItem = document.createElement('li');
+            listItem = document.createElement('li');
             listItem.textContent = optionElements[j].value;
+            listItem.tabIndex = 0;
+            listItem.addEventListener('click', function(e) {
+                selectUpdateValue(e);
+            });
             optsList.appendChild(listItem);
         }
+
+        // open and close the list by setting aria-expanded
+        function selectOpenList(e) {
+            var target;
+            if (e.target) {
+                target = e.target;
+            } else {
+                target = e;
+            }
+            var expandedState = target.getAttribute('aria-expanded');
+            if (expandedState === 'true') {
+                expandedState = 'false';
+            } else {
+                expandedState = 'true';
+            }
+            target.setAttribute('aria-expanded', expandedState);
+        }
+
+        // update entry point text and value of underlying select element
+        function selectUpdateValue(e) {
+            var target = e.target;
+            var value = target.textContent;
+            var thisSelectElement = target.parentNode.parentNode.querySelector('select');
+            var thisEntryPoint = target.parentNode.previousElementSibling;
+            thisSelectElement.value = value;
+            thisEntryPoint.textContent = value;
+
+            selectOpenList(thisEntryPoint);
+        }
     }
-}
-selectReplace();
+}());
+
+// function to call the Intent API and display the distance
+(function calculateDistance() {
+    var calcBtn = document.getElementById('calcBtn');
+    var resultContainer = document.getElementById('resultContainer');
+
+    calcBtn.addEventListener('click', function() {
+        var fromValue = document.querySelector('#selectFrom select').value;
+        var toValue = document.querySelector('#selectTo select').value;
+        var result = IntentMedia.Distances.distance_between_airports(fromValue, toValue);
+
+        resultContainer.textContent = result;
+    });
+}());
